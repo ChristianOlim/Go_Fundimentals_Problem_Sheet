d1def97e2a3b96778d1e295a065ae0242b2ff123
ChristianOlim <christianolim@hotmail.com>
2017-10-06 21:49:41 +0100

    update to FizzBuzz

--- 03_FizzBuzz.go
+++ 03_FizzBuzz.go
@@ -9,25 +9,21 @@ import (
 
 func main() {  
 	
-	//Printing numbers from 1 to 100
+    //Printing numbers from 1 to 100
     for i := 1; i <= 100; i++ {
 
-		//This method solves 'FizzBuzz' for multiples of 3 and 5
-        if i%15 == 0 {
-            fmt.Println("FizzBuzz")
-		}
-		//This if statement prints 'Fizz' for multiples of 3
-		else if i%3 == 0 {
-            fmt.Println("Fizz")
-		}
-		//This if statement prints 'Buzz' for multiples of 5
-		else if i%5 == 0 {
-            fmt.Println("Buzz")
-		}
-		//This will print out the number if it's not divisible by 3 and/ or 5
-		else {
-            fmt.Println(i)
-		}
+	//This if statement solves 'FizzBuzz' for multiples 
+	//of 3 and 5. It also printing 'Fizz' for muliples
+	//of 3 and prints 'Buzz' for multiples of 5.
+      if i%15 == 0 {
+	fmt.Println("FizzBuzz")
+	} else if i%3 == 0 {
+	fmt.Println("Fizz")
+	} else if i%5 == 0 {
+	fmt.Println("Buzz")
+	} else {
+	fmt.Println(i)
+	}
 		
     }
 }
